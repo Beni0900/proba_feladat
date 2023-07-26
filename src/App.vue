ee4d24b0dc8edec992dfede6898157ef97d43184
Beni0900 <74322095+Beni0900@users.noreply.github.com>
2023-07-26 23:19:56 +0200

    Skeleton created
    Skeleton created and gave some scss for this. Tried with test datas, with test backgrounds

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
 <template>
   <Button />
-  <div class="container"></div>
+  <div class="container">
+    <div class="header">
+      <p></p>
+    </div>
+    <div class="center">
+      <p></p>
+    </div>
+    <div class="bottom">
+      <div>
+        <p></p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -14,4 +26,32 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style lang="scss">
+.container {
+  width: 800px;
+  height: 300px;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: 110px 60px 110px;
+  gap: 10px;
+  row-gap: 10px;
+  padding: 10px;
+  grid-template-areas:
+    "header header header"
+    "center center center"
+    "bottom bottom bottom";
+
+  .header {
+    grid-area: header;
+  }
+  .center {
+    grid-area: center;
+  }
+  .bottom {
+    grid-area: bottom;
+    display: grid;
+    grid-template-rows: repeat(1, 1fr);
+    gap: 10px;
+  }
+}
+</style>
